a8e28b2ef9731d0748d885c54c5d3ff10155949b
YuriyTG <91891311+YuriyTG@users.noreply.github.com>
2022-07-11 22:59:53 +0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to codingame-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/codingame_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns codingame-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [codingame-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
